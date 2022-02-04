7aee14036b8003bd5bd216b0b24dad5f30499846
Максим Бельфер <maxbelf1@gmail.com>
2022-02-04 15:58:54 +0300

    feat: added search input

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,11 @@
 			<Meetings />
 		</div>
 		<div class="block">
+			<div class="search">
+				<input type="text" placeholder="Search" />
+			</div>
+			<Welcome />
+
 			{{ counter }}
 
 			<button @click="increment">GG</button>
@@ -32,6 +37,11 @@ definePageMeta({
 	flex-direction: row;
 	justify-content: flex-start;
 	flex-wrap: nowrap;
+	width: 100%;
+
+	@media (max-width: 1023px) {
+		flex-direction: column;
+	}
 	.content {
 		padding: 27px 70px;
 		border: 2px solid #fff;
@@ -39,6 +49,31 @@ definePageMeta({
 		background: linear-gradient(107.56deg, rgba(255, 255, 255, 0.8) 0%, rgba(255, 255, 255, 0.2) 100%);
 		height: 100vh;
 		overflow: scroll;
+	}
+	.block {
+		flex: 1;
+		padding: 32px 65px;
+		.search {
+			display: flex;
+			justify-content: flex-end;
+			input {
+				width: 311px;
+				height: 51px;
+				display: flex;
+				align-items: center;
+				padding: 0 18px;
+				border-radius: 30px;
+				font-weight: 500;
+				font-size: 20px;
+				line-height: 70.19%;
+				/* identical to box height, or 14px */
+				letter-spacing: -0.075em;
+				text-transform: capitalize;
+				border: 2px solid #fff;
+				color: rgba(250, 144, 126, 0.49);
+				background: linear-gradient(111.15deg, rgba(255, 255, 255, 0.64) 0%, rgba(255, 255, 255, 0.16) 100%);
+			}
+		}
 	}
 }
 </style>
